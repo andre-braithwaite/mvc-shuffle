f3b1c0a02b52da0c43186e7c81dfcc09e9e4767d
andre <andrebwt@gmail.com>
2017-08-12 00:22:03 +0300

    Started adapting login to MVC framework.

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -6,11 +6,54 @@ class LoginController extends Controller {
         View::render('LoginView.php');
     }
 
+    function userFound($xml, $username) {
+
+        $userExists = false;
+
+        foreach ($xml->children() as $user) {
+            if ($username == $user->username) {
+                $userExists = true;
+            }
+        }
+
+        if ($userExists) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function passCorrect($xml, $username, $password) {
+
+        $goodPass = false;
+
+        foreach ($xml->children() as $user) {
+            if ($username == $user->username && $password == $user->password) {
+                $goodPass = true;
+            }
+        }
+
+        if ($goodPass) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     function processLogin() {
+
         $username = $_REQUEST['username'];
         $password = $_REQUEST['password'];
+        $do_login = false;
 
-        echo 'username entered: ' . $username . '<br>';
-        echo 'password entered: ' . $password;
+
+        if(file_exists('../../../models/tests/user-logins/test-users.xml')){
+
+        } else {
+            echo 'User Database not found!';
+        }
+
+        //echo 'username entered: ' . $username . '<br>';
+        //echo 'password entered: ' . $password;
     }
 }
